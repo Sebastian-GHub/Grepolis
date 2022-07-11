e8e306b9d63f76efce341af29808ce40645c22ff
Sebastian <Sebastian@User-PC.speedport.ip>
2022-07-11 20:34:30 +0200

    Updates to functionality - close to working

--- Grepolis-Bot/src/www/grepolis/com/plugins/VillageFarmer.java
+++ Grepolis-Bot/src/www/grepolis/com/plugins/VillageFarmer.java
@@ -1,6 +1,11 @@
 package www.grepolis.com.plugins;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class VillageFarmer extends BotPlugin{
 	private WebDriver driver;
@@ -13,8 +18,7 @@ public class VillageFarmer extends BotPlugin{
 	
 	@Override
 	public void startUp() {
-		//TODO actual code for this startup
-		//Ehh just need to wait for my account to get 2 cities
+		openMenu();
 		
 		
 	}
@@ -25,6 +29,40 @@ public class VillageFarmer extends BotPlugin{
 	 */
 	public void getStatus() {
 		//TODO check method description
+	}
+	
+	public void openMenu() {
+		Actions builder = new Actions(driver);
+		WebElement overview = driver.findElement(By.xpath("/html/body/div[1]/div[14]/div[3]/div"));
+		builder.moveToElement(overview).perform();
+		WebElement villageOverview = driver.findElement(By.xpath("/html/body/div[8]/div[2]/div/div/ul/li[2]/ul/li[2]/a"));
+		villageOverview.click();
+		
+		//Debug and testing section starts here:
+		//Current testing goal: Find the amount of islands/
+		List<WebElement> el = driver.findElements(By.tagName("div"));
+		WebElement bauernDörfer = null;
+		for(WebElement e : el) {
+			if(e.getAccessibleName().equals("Bauerndörfer")) {
+				bauernDörfer = e;
+				break;
+			}
+		}
+		el.clear();
+		//el = bauernDörfer.findElements(By.tagName("div"));
+		WebElement sideSelect = bauernDörfer.findElement(By.id("fto_town_wrapper"));
+		WebElement subSideSelect = sideSelect.findElement(By.id("fto_town_list"));
+		el = subSideSelect.findElements(By.tagName("li"));
+		int islands = 0;
+		
+		
+		//Lists all elements in the subSideSelect which contains: islands and cities
+		//Think of a way to distinguish each from another
+		for(WebElement e : el) {
+			System.out.println(e.getText());
+			System.out.println("");
+		}
+		
 	}
 
 

e1a9113977fbbdb4dc37f7b0a3f1384e57f7a0f5
Sebastian <Sebastian@User-PC.speedport.ip>
2022-07-12 11:22:03 +0200

    Added basic mode (works, no long testing)

--- Grepolis-Bot/src/www/grepolis/com/plugins/VillageFarmer.java
+++ Grepolis-Bot/src/www/grepolis/com/plugins/VillageFarmer.java
@@ -1,14 +1,17 @@
 package www.grepolis.com.plugins;
 
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-public class VillageFarmer extends BotPlugin{
+public class VillageFarmer extends BotPlugin implements Runnable{
 	private WebDriver driver;
+	public int mode;
 	
 	public VillageFarmer(WebDriver driver) {
 		this.driver = driver;
@@ -18,9 +21,21 @@ public class VillageFarmer extends BotPlugin{
 	
 	@Override
 	public void startUp() {
-		openMenu();
-		
-		
+		System.out.println("Select the plugins mode:\n1. Basic mode");
+		Scanner s = new Scanner(System.in);
+		mode = s.nextInt();
+		s.close();
+
+	}
+	
+	@Override
+	public void run() {
+		if(mode == 1) {
+			basicMode();
+		}else {
+			System.out.println("Not a mode, terminating");
+			System.exit(0);
+		}
 	}
 	
 	/**
@@ -36,8 +51,31 @@ public class VillageFarmer extends BotPlugin{
 		WebElement overview = driver.findElement(By.xpath("/html/body/div[1]/div[14]/div[3]/div"));
 		builder.moveToElement(overview).perform();
 		WebElement villageOverview = driver.findElement(By.xpath("/html/body/div[8]/div[2]/div/div/ul/li[2]/ul/li[2]/a"));
-		villageOverview.click();
+		villageOverview.click();	
+	}
+	
+	public void basicMode() {
+		while(true) {
+			openMenu();
+			WebElement checkBox = driver.findElement(By.xpath("/html/body/div[14]/div[2]/div[5]/div[1]/div/div[9]/span[2]/a"));
+			checkBox.click();
+			WebElement button = driver.findElement(By.xpath("/html/body/div[14]/div[2]/div[5]/div[2]/div[2]/div/ul/li[2]/div[2]/div[3]"));
+			button.click();
+			try {
+				Random rn = new Random();
+				int rndmDelay = rn.nextInt(315000 - 300000 + 1) + 300000;
+				Thread.sleep(rndmDelay);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
 		
+	}
+	
+	
+	public void testing() {
 		//Debug and testing section starts here:
 		//Current testing goal: Find the amount of islands/
 		List<WebElement> el = driver.findElements(By.tagName("div"));
@@ -53,16 +91,14 @@ public class VillageFarmer extends BotPlugin{
 		WebElement sideSelect = bauernDörfer.findElement(By.id("fto_town_wrapper"));
 		WebElement subSideSelect = sideSelect.findElement(By.id("fto_town_list"));
 		el = subSideSelect.findElements(By.tagName("li"));
-		int islands = 0;
-		
-		
+		int islands = 0;	
+			
 		//Lists all elements in the subSideSelect which contains: islands and cities
 		//Think of a way to distinguish each from another
 		for(WebElement e : el) {
 			System.out.println(e.getText());
 			System.out.println("");
 		}
-		
 	}
 
 

59ea1f373e6258af5184b4152e187dd4e9d3d7ee
Sebastian <Sebastian@User-PC.speedport.ip>
2022-07-12 11:21:32 +0200

    Thread functionality

--- Grepolis-Bot/src/www/grepolis/com/plugins/BotPlugin.java
+++ Grepolis-Bot/src/www/grepolis/com/plugins/BotPlugin.java
@@ -1,12 +1,17 @@
 package www.grepolis.com.plugins;
 
-public abstract class BotPlugin {
+public abstract class BotPlugin implements Runnable{
 	String name;
 	String version;
 	
 	public abstract void startUp();
+	public abstract void run();
 	
 	public String getInfo() {
 		return "Plugin: "+ name + " Version: " + version;
+	}
+	
+	public void exitMenu() {
+		//TODO
 	}
 }
